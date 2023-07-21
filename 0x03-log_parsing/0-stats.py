3046055574f7970ea574c92b0eb254dc411b955d
muhdmaahee2000 <youngzmahi99@gmail.com>
2023-07-21 15:50:01 +0100

    read stdin of a user line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,48 @@
-#!/usr/bin/python3
-"""Performs log parsing from stdin"""
-
-import re
 import sys
-counter = 0
-file_size = 0
-statusC_counter = {200: 0, 301: 0, 400: 0,
-                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
+def print_statistics(total_size, status_codes):
+    print(f"Total file size: {total_size}")
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code] > 0:
+            print(f"{status_code}: {status_codes[status_code]}")
 
-def printCodes(dict, file_s):
-    """Prints the status code and the number of times they appear"""
-    print("File size: {}".format(file_s))
-    for key in sorted(dict.keys()):
-        if statusC_counter[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+def main():
+    total_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
+    try:
+        for line_number, line in enumerate(sys.stdin, start=1):
+            line = line.strip()
+
+            # Split the line into parts based on spaces
+            parts = line.split()
+
+            # Check if the line contains all the required components
+            if len(parts) != 10:
+                continue
+
+            # Extract the file size and status code from the line
+            try:
+                file_size = int(parts[-1])
+                status_code = parts[-2]
+
+                # Update the total size and status code count
+                total_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+            except ValueError:
+                # If file size or status code is not an integer, skip the line
+                continue
+
+            # Print statistics every 10 lines
+            if line_number % 10 == 0:
+                print_statistics(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        # Print statistics when CTRL + C is pressed
+        print_statistics(total_size, status_codes)
 
 if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            split_string = re.split('- |"|"| " " ', str(line))
-            statusC_and_file_s = split_string[-1]
-            if counter != 0 and counter % 10 == 0:
-                printCodes(statusC_counter, file_size)
-            counter = counter + 1
-            try:
-                statusC = int(statusC_and_file_s.split()[0])
-                f_size = int(statusC_and_file_s.split()[1])
-                # print("Status Code {} size {}".format(statusC, f_size))
-                if statusC in statusC_counter:
-                    statusC_counter[statusC] += 1
-                file_size = file_size + f_size
-            except:
-                pass
-        printCodes(statusC_counter, file_size)
-    except KeyboardInterrupt:
-        printCodes(statusC_counter, file_size)
-        raise
+    main()
+
